adf2ff8a044b3fa3f9e05675bfb9710dc99c1d62
camevans87 <56937461+camevans87@users.noreply.github.com>
2021-04-02 12:07:47 -0400

    initial commit

--- src/services/imageServices.js
+++ src/services/imageServices.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
+//cloud server
+const apiUrl = 'https://ancient-brushlands-03812.herokuapp.com';
+
+//localhost server (uncomment to use)
+//const apiUrl = "https://localhost:4000"
+
 export const fetchImages = async () => {
-  const res = await axios.get('http://localhost:4000/images');
+  const res = await axios.get(`${apiUrl}/images`);
   return res.data;
 };
 
 export const deleteImage = async (id) => {
-  const res = await axios.delete(`http://localhost:4000/images/${id}`);
+  const res = await axios.delete(`${apiUrl}/images/${id}`);
   return res;
 };
 
 export const addImage = async (newImage) => {
   console.log('Add image fired');
-  const res = await axios.post('http://localhost:4000/images', newImage);
+  const res = await axios.post(`${apiUrl}/images`, newImage);
   console.log(res);
   return res;
 };
